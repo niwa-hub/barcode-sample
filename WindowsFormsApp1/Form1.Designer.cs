322729adec33a1f1e3df85e375cde7099de05626
ニワ ヒロキ <niwa00028@yahoo.co.jp>
2021-03-27 16:06:40 +0900

    commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MovieBox = new System.Windows.Forms.PictureBox();
+            this.CodeBox = new System.Windows.Forms.PictureBox();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.ChangeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MovieBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CodeBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MovieBox
+            // 
+            this.MovieBox.Location = new System.Drawing.Point(12, 12);
+            this.MovieBox.Name = "MovieBox";
+            this.MovieBox.Size = new System.Drawing.Size(500, 500);
+            this.MovieBox.TabIndex = 0;
+            this.MovieBox.TabStop = false;
+            // 
+            // CodeBox
+            // 
+            this.CodeBox.Location = new System.Drawing.Point(599, 12);
+            this.CodeBox.Name = "CodeBox";
+            this.CodeBox.Size = new System.Drawing.Size(500, 500);
+            this.CodeBox.TabIndex = 1;
+            this.CodeBox.TabStop = false;
+            // 
+            // TextBox
+            // 
+            this.TextBox.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBox.Location = new System.Drawing.Point(599, 518);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(500, 74);
+            this.TextBox.TabIndex = 2;
+            // 
+            // ChangeButton
+            // 
+            this.ChangeButton.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ChangeButton.Location = new System.Drawing.Point(146, 533);
+            this.ChangeButton.Name = "ChangeButton";
+            this.ChangeButton.Size = new System.Drawing.Size(219, 59);
+            this.ChangeButton.TabIndex = 3;
+            this.ChangeButton.Text = "カメラ切り替え";
+            this.ChangeButton.UseVisualStyleBackColor = true;
+            this.ChangeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 604);
+            this.Controls.Add(this.ChangeButton);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.CodeBox);
+            this.Controls.Add(this.MovieBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MovieBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CodeBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MovieBox;
+        private System.Windows.Forms.PictureBox CodeBox;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Button ChangeButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool isStart = false;
+        bool isRun = false;
+
+        int camera = 0;
+
+        private List<int> cameras = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    using (VideoCapture videoCapture = new VideoCapture()) 
+                    {
+                        if(videoCapture.Open(i))
+                        {
+                            cameras.Add(i);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            if(cameras.Count <= 1)
+            {
+                ChangeButton.Enabled = false;
+            }
+            Start();
+        }
+
+        private async void Start()
+        {
+            isStart = true;
+            isRun = true;
+            await Task.Run(() =>
+            {
+                using (VideoCapture videoCapture = new VideoCapture())
+                {
+                    videoCapture.Open(camera);
+                    while (isStart)
+                    {
+                        using (Mat src = new Mat())
+                        {
+                            if (videoCapture.Read(src))
+                            {
+                                using (Stream stream = src.Resize(new OpenCvSharp.Size(MovieBox.Width, MovieBox.Height)).ToMemoryStream())
+                                {
+                                    Image img = Image.FromStream(stream);
+                                    MovieBox.Image = new Bitmap(img);
+
+                                    Task.Run(() =>
+                                    {
+                                        BarcodeReader reader = new BarcodeReader();
+                                        Result result = reader.Decode(new Bitmap(img));
+                                        if (result != null)
+                                        {
+                                            CodeBox.Image = img;
+                                            Invoke(new MethodInvoker(() => { TextBox.Text = result.Text; }));
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+                isRun = false;
+            });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isStart = false;
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            isStart = false;
+
+            // 終了待ちして再開
+            while (isRun) { };
+
+            int index = cameras.IndexOf(camera) + 1;
+            if(index == cameras.Count)
+            {
+                index = 0;
+            }
+            camera = cameras[index];
+            Start();
+        }
+    }
+}
